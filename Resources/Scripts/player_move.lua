1bf89a9a67ffb0bebd0b9d223796da41792d2d14
brady <bradymc1998@gmail.com>
2022-02-27 15:35:52 -0800

    commented out controlller behavior script

--- Resources/Scripts/player_move.lua
+++ Resources/Scripts/player_move.lua
@@ -1,11 +1,11 @@
 -- if D is pressed, go right
-if is_pressed(7) then move(200.0) end
+--if is_pressed(7) then move(200.0) end
 -- if A is pressed, go left
-if is_pressed(4) then move(-200.0) end
+--if is_pressed(4) then move(-200.0) end
 -- if SPACE is pressed, jump
-if is_pressed(44) then 
-	set_grav(true)
-	jump(-100.0) 
-end
+--if is_pressed(44) then 
+--	set_grav(true)
+--	jump(-100.0) 
+--end
 -- if A or D are released, stop moving
-if is_released(7) or is_released(4) then move(0.0) end
+--if is_released(7) or is_released(4) then move(0.0) end
